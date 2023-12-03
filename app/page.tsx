111f79d7f0b6b3603d70b30fe3ab15cbc93ce230
pritamcn <you@example.com>
2023-12-03 20:11:15 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+import RhfWithAction from '@/components/with-action'
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='mb-16 text-2xl font-medium'>React Hook Forms</h1>
+        <RhfWithAction />
+      </div>
+    </section>
+  )
+}
